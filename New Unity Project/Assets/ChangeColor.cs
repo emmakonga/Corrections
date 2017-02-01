4188a5b833aaec48a8461af9a9408c094f52a816
Unknown <emmanymakonga@gmail.com>
2017-02-01 11:23:23 -0700

    Syntax

---
+++ New Unity Project/Assets/ChangeColor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeColor : MonoBehaviour {
+	// Use this for initialization
+
+	void Start () {
+		if (input.GetDown (KeyCode.R))
+			GetComponent<Renderer> ().material.color = Color.red;
+				print ("Changed color to red.");
+
+
+	  //turns object GGREEN
+	if(input.GetKeyDown(KeyCode.G))
+}
+GetComponent<Renderer>().material.color = color.green;
+	Print("Changed color to green";
+
---
+++ New Unity Project/Assets/Syntax1.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Syntax1 : MonoBehaviour {
+public string myString = "Hello word";
+public string myOtherString = "1";
+    public int myInt = 1;
+    public float myFloat = 1.2f;
+	// Use this for initialization
+	void Start () {
+	print("Hello");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
